d89df1176912d5cb51c047274d1b3b132b2303e1
abdul1ah <almamun01817@gmail.com>
2023-05-17 23:34:35 +0600

    login page

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,21 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Register from "./Components/Register/Register";
+import Login from "./Components/Login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element:<App></App>
+  },
+  {
+    path: "/register",
+    element:<Register></Register>
+  },
+  {
+    path: "/login",
+    element:<Login></Login>
   },
 ]);
 
